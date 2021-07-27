b35396d0644f0df0c708be2ea2177ce6888b2036
CHERVA <88034188+CHERVA@users.noreply.github.com>
2021-07-27 16:18:04 +0300

    Add files via upload

---
+++ grabcaminstaller.bash
@@ -0,0 +1,12 @@
+sudo apt update && upgrade
+clear
+printf "      \e[1;77m Идет загрузка скрипта подождите пожалуста\e[0m \n"
+#типа с моего гитхаба загрузка пойдет ок да?
+pip install lolcat
+clear
+pip install php
+clear
+git clone https://github.com/CHERVA/GRABCAMFORUKRAINE
+cd GRABCAMFORUKRAINE
+clear
+bash grabcam.sh
